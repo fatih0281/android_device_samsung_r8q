f612021f04af0b073d517023acaac33b5090db6d
fatih0281 <124438691+fatih0281@users.noreply.github.com>
2026-06-30 00:16:37 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-yaap
+    $(LOCAL_PATH)/overlay-lineage
 
 # call the common setup
 $(call inherit-product, device/samsung/sm8250-common/common.mk)
