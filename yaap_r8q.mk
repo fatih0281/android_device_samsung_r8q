1b542e9ce681c6f755f7a359e07a10d1e53f5922
fatih0281 <124438691+fatih0281@users.noreply.github.com>
2026-06-30 15:06:54 +0300

    Update and rename custom_r8q.mk to yaap_r8q.mk

--- yaap_r8q.mk
+++ yaap_r8q.mk
@@ -23,14 +23,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/r8q/device.mk)
 
 # Inherit some common YAAP stuff
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := custom_r8q
+PRODUCT_NAME := yaap_r8q
 PRODUCT_DEVICE := r8q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G780G
